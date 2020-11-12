373947609bafe32e1f607b9ae0363410ec82398d
Mark <Marcusa.helms@gmail.com>
2020-11-11 22:04:08 -0600

    yes

---
+++ Game.js
@@ -0,0 +1,12 @@
+
+/**const reverseString = (str)=>{
+    let reversed = "";
+    for(let i = str.length -1; i>=0; i--){
+        reversed += str[i];
+    }
+    console.log(reversed);
+}
+
+reverseString("words");
+ */
+
